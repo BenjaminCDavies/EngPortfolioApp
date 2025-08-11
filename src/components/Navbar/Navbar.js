ffcb7d23c0000106393f521b53045e0bb441f08e
Benjamin Davies <benjamin@the-davies.net>
2025-08-11 20:19:58 +0930

    Logout functionality
    Added a logout button to the navbar so that I don't need to keep clearing the authentication fro inspect element.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,30 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { supabase } from '../../database/supabaseClient';
 import './Navbar.css'
 
 function Navbar() {
-    return (
-      <nav className="navbar">
-        <a href="/profile">Profile</a>
-        <a href="/dashboard">Dashboard</a>
-        <a href="/goals">Goals</a>
-        <a href="/networking">Networking</a>
-        <a href="/competencies">Competencies</a>
-        <a href="/cdl">CDL</a>
-      </nav>
-    );
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    navigate('/login');
+  }
+
+  return (
+    <nav className="navbar">
+      <Link to="/profile">Profile</Link>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/goals">Goals</Link>
+      <Link to="/networking">Networking</Link>
+      <Link to="/competencies">Competencies</Link>
+      <Link to="/cdl">CDL</Link>
+
+      {/* logout button */}
+      <button onClick={handleLogout} className="logout-button">
+        Logout
+      </button>
+    </nav>
+  );
 }
 
 export default Navbar
