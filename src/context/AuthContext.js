3388bb4563254f95bcbad9a582ca73b67a394615
Benjamin Davies <benjamin@the-davies.net>
2025-08-14 09:00:40 +0930

    Commenting auth context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,32 +1,46 @@
 import { createContext, useState, useEffect, useContext } from 'react';
 import { supabase } from '../database/supabaseClient';
 
+// context that will carry data
 const AuthContext = createContext();
 
+// anything inside children can read authorisation data
 export function AuthProvider({ children }) {
+
+  // state to hold current user object
   const [user, setUser] = useState(null);
+
+  // state for if we are figuring out authorisation
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+
+    // ask supabase for the current session
     const session = supabase.auth.getSession().then(({ data }) => {
+
+      // store the user or store null
       setUser(data.session?.user ?? null);
       setLoading(false);
     });
 
     // check to see if user logs in or out
+    // a subscription - if an event happens, supabase gives a new session
     const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
       setUser(session?.user ?? null);
     });
 
+    // unsubscribe when useEffect is done
     return () => {
       listener.subscription.unsubscribe();
     };
   }, []);
 
+
   return (
-    <AuthContext.Provider value={{ user, loading }}>
+    // user/null and whether authorisation state is still being determined
+    <AuthContext.Provider value={{ user, loading }}> 
       {children}
-    </AuthContext.Provider>
+    </AuthContext.Provider> // comes from createContest()
   );
 }
 
